f7c58862d30a2c8510447263bf5c302d3d3ca069
airy <airy@gaia.(none)>
2009-09-12 01:25:26 +0300

    added generate_code method to AR::Base
    with this method, it's easier to create unique codes for records.

---
+++ lib/ar_code_generate.rb
@@ -0,0 +1,7 @@
+class ActiveRecord::Base 
+  # Generates a pseudo-random string in hex format (0..9+A..F)
+  # which contains chunk*16 bits of randomness.
+  def generate_code(chunks=2)
+    ("%04x"*chunks % ([nil]*chunks).map { rand(2**16) }).upcase
+  end
+end
