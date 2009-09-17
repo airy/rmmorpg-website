8892390acd9e0d06d3846531c0d0bfade8676c6a
airy <airy@gaia.(none)>
2009-09-17 21:34:43 +0300

    lookup method added to application helper
    for foreign key lookup combos on forms.
    TODO: need to integrate rails formhelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+	def lookup(f, field, model, include_blank = false)
+		 f.select field, model.find(:all).collect  {|p| [ p.name, p.id ] },{ :include_blank => include_blank }
+	end
 end
