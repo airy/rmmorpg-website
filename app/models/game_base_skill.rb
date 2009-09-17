0338b89c44226d75539ab0943a1f662f4af5059b
airy <airy@gaia.(none)>
2009-09-17 23:07:44 +0300

    GameBaseSkillReagants relation added to GameBaseSkill Class

--- app/models/game_base_skill.rb
+++ app/models/game_base_skill.rb
@@ -1,4 +1,8 @@
 class GameBaseSkill < ActiveRecord::Base
+	has_many :game_base_skill_reagents
+	#has_many :reagents, :through => :game_base_skill_reagents, :class_name => :game_base_items
+
+
   before_create :get_code
 
 private
