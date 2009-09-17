a060bc1bb17d3b99d6e36538b5b613ba08351b0e
airy <airy@gaia.(none)>
2009-09-17 23:25:34 +0300

    slot class changesssssss

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -3,7 +3,25 @@ class Slot < ActiveRecord::Base
 	belongs_to :game_base_slot
 	has_one :item, :as => :container, :dependent => :destroy
 	
+	#belongs_to :game_base_skill, :through => :item, :include => :game_base_item
+	
+	validates_uniqueness_of :game_base_slot_id, :scope => :char_id
+	
+	
+	
+	delegate :slot_type, 
+		:accepts, 
+		:birth_right,
+		:icon, 
+	:to => :game_base_slot
+	
+	#delegate :game_base_skill, :to => :item
+	
+
+	
 	named_scope :by_type, lambda { |t| { :joins => :game_base_slot, :conditions => ["slot_type = ?", t]}}
+	
+	
 	def current_icon
 		if item && item.icon
 			item.icon 		
