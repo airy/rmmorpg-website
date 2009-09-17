02113cad0d69b294322df793a5f4a8efb0ca7388
airy <airy@gaia.(none)>
2009-09-17 23:25:18 +0300

    added dummy methods to inspect/improve the wuxai system

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,11 @@ module ApplicationHelper
 	def lookup(f, field, model, include_blank = false)
 		 f.select field, model.find(:all).collect  {|p| [ p.name, p.id ] },{ :include_blank => include_blank }
 	end
+	
 	def iconify icon
 		image_tag "/images/game-icons/#{icon}" if icon
 	end	
+	
 	
 	def wuxai count, modifier = 0
 		arr = count.d10.to_a
@@ -16,4 +18,11 @@ module ApplicationHelper
 		return ( max * 10 ) + num + modifier
 	end
 	
+	def hit_crit_check
+		1.d100.roll
+	end
+	
+	def leveler
+		3.d10 == 30
+	end
 end
