b946ce3f8a5816b73dcbd32671fd934966c50602
airy <airy@gaia.(none)>
2009-09-17 22:33:37 +0300

    wuxai method added.
    this method helps rolls for using abilities etc.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
 	def iconify icon
 		image_tag "/images/game-icons/#{icon}" if icon
 	end	
+	
+	def wuxai count, modifier = 0
+		arr = count.d10.to_a
+		freq = arr.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+		max = freq.values.max                   # we're only interested in the keys with the highest frequency
+		freq.select { |k, f| f == max }         # extract the keys that have the max frequency
+		num = arr.sort_by { |v| freq[v] }.last
+		return ( max * 10 ) + num + modifier
+	end
+	
 end
