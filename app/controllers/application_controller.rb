de5f564846066d47ba0d785f93a6b66215df07cd
airy <airy@gaia.(none)>
2009-09-17 21:53:49 +0300

    require dice.rb added to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-require 'lib/ar_code_generate.rb'
+require 'ar_code_generate.rb'
+require 'dice.rb'
+
 class ApplicationController < ActionController::Base
 	helper :all # include all helpers, all the time
 	protect_from_forgery # See ActionController::RequestForgeryProtection for details
